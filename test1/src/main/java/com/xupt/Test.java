2fd3fba2004030dd9603034534ab6e6299653683
gjy <gjy@xupt.com>
2022-05-14 19:05:33 +0800

    second commit

--- test1/src/main/java/com/xupt/Test.java
+++ test1/src/main/java/com/xupt/Test.java
@@ -3,5 +3,6 @@ package com.xupt;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!2");
     }
 }

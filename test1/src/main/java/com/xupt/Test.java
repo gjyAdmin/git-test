a0368f05e479ddbd6874468d229e8ce99dc0b358
gjy <gjy@xupt.com>
2022-05-14 19:21:17 +0800

    hot-fix commit

--- test1/src/main/java/com/xupt/Test.java
+++ test1/src/main/java/com/xupt/Test.java
@@ -5,5 +5,6 @@ public class Test {
         System.out.println("Hello World!");
         System.out.println("Hello World!2");
         System.out.println("Hello World!3");
+        System.out.println("hot-fix commit");
     }
 }
